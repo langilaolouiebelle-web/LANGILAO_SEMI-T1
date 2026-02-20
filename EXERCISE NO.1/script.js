8d8938d4c11fe6d3d72b3c3bcf36ccc8d1a0b3fb
langilaolouiebelle-web <langilaolouiebelle@gmail.com>
2026-02-20 18:07:51 +0800

    Update script.js

--- EXERCISE NO.1/script.js
+++ EXERCISE NO.1/script.js
@@ -1,22 +1,18 @@
 function calculate() {
 
-    // kuhaon ang value sa input
     var num1 = parseFloat(document.getElementById("num1").value);
     var num2 = parseFloat(document.getElementById("num2").value);
 
-    // check kung walay sulod
     if (isNaN(num1) || isNaN(num2)) {
         alert("Please enter both numbers!");
         return;
     }
-
-    // operations
+    
     var sum = num1 + num2;
     var difference = num1 - num2;
     var product = num1 * num2;
     var quotient = num2 !== 0 ? num1 / num2 : "Cannot divide by zero";
 
-    // display results automatically
     document.getElementById("sum").innerHTML = sum;
     document.getElementById("difference").innerHTML = difference;
     document.getElementById("product").innerHTML = product;
@@ -25,11 +21,9 @@ function calculate() {
 
 function clearFields() {
 
-    // clear inputs
     document.getElementById("num1").value = "";
     document.getElementById("num2").value = "";
 
-    // clear results
     document.getElementById("sum").innerHTML = "";
     document.getElementById("difference").innerHTML = "";
     document.getElementById("product").innerHTML = "";
